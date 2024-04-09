2563f9c4477a1ece24dfd0cc754f35aab005a4fe
ajli911 <164284583+ajli911@users.noreply.github.com>
2024-04-09 04:23:33 +0000

    complete

--- app.rb
+++ app.rb
@@ -10,3 +10,51 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 get("/") do
   erb(:homepage)
 end
+
+get("/rock") do
+  options = ["rock", "paper", "scissors"]
+
+  @cpu = options.sample
+
+  if @cpu == "rock"
+    @outcome = "tied"
+  elsif @cpu == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  options = ["rock", "paper", "scissors"]
+
+  @cpu = options.sample
+
+  if @cpu == "rock"
+    @outcome = "won"
+  elsif @cpu == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  options = ["rock", "paper", "scissors"]
+
+  @cpu = options.sample
+
+  if @cpu == "rock"
+    @outcome = "lost"
+  elsif @cpu == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
+end
